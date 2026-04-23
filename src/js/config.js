74fee6f6f7174c66b06a1d07e3318714db0a271e
renyu16 <rztiangaorwc@163.com>
2026-04-23 15:03:14 +0800

    update src file from pakeplus

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+// 前端配置
+// 修改 API 地址为你的服务器地址
+const API_CONFIG = {
+    // 服务器地址，如果是本地开发用 localhost，生产环境用服务器 IP 或域名
+    apiUrl: 'http://127.0.0.1:8082',
+    
+    // 是否使用本地配置（true=使用上面的apiUrl，false=自动获取当前页面地址）
+    useLocalConfig: true
+};
